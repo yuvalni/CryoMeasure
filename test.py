3360091fdad0f81c838346e70a51093ee249da5c
yuvalni <original.pr@gmail.com>
2022-01-06 13:20:58 +0200

    fixed polynomails things

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from Keithley196V2 import Keithley196
 k196 = Keithley196()
 from CryoMeasure import *
-start_cont_measure(10**-5,2,5,'second_cooling_measurement_roughing_pump',0.05,k196,AC=False)
+start_cont_measure(10**-5,2,5,'third_cooling_turbo',0.05,k196,AC=False)
